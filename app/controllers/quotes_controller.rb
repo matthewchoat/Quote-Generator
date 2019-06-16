021daf0de5e9b7ea9260011afa9f5ecd4771a387
matthewchoat <matthewchoat@gmail.com>
2019-06-16 20:34:44 +0000

    Splurty lesson 13

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,18 @@ class QuotesController < ApplicationController
   def new
     @quote = Quote.new
   end
+
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
